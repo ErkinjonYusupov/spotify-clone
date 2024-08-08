9cf9d90c147d81349d2e276f28b4030b09cdc1d2
Yusupov Erkinjon <erkinnigoradiyora@gmail.com>
2024-08-08 18:00:36 +0500

    add: splash page

---
+++ lib/presentation/splash/pages/splash.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:spotify/core/config/assets/app_vectors.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SvgPicture.asset(AppVectors.logo),
+      ),
+    );
+  }
+}
